9423a30fa48f36ddb90ba2be4520b409f8eb1ce6
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2024-08-04 12:01:25 +0530

    setup done

---
+++ sdocs/app/layout.tsx
@@ -0,0 +1,34 @@
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { Metadata } from 'next';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'SyncDocs',
+  description: 'Your goto collaborative docs',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
